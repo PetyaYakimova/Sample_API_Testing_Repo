01935556786beeb0dc757256b6b98475f3774f2f
Petya Yakimova <petya_yakimova@abv.bg>
2026-04-12 13:16:44 +0300

    Add api client

---
+++ ApiTests/Clients/ApiClient.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+public class ApiClient
+{
+    private readonly HttpClient _httpClient;
+
+    public ApiClient(string baseUrl)
+    {
+        _httpClient = new HttpClient
+        {
+            BaseAddress = new System.Uri(baseUrl)
+        };
+    }
+
+    public async Task<HttpResponseMessage> GetAsync(string endpoint)
+    {
+        return await _httpClient.GetAsync(endpoint);
+    }
+
+    public async Task<HttpResponseMessage> PostAsync(string endpoint, string jsonBody)
+    {
+        var content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
+        return await _httpClient.PostAsync(endpoint, content);
+    }
+}
