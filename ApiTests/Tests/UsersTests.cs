221ca4e2969d2aedd0dffec01abfa7dd2b851cbe
Petya Yakimova <petya_yakimova@abv.bg>
2026-04-13 13:15:39 +0300

    Add user test

---
+++ ApiTests/Tests/UsersTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using FluentAssertions;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+[TestFixture]
+public class UsersTests
+{
+    private ApiClient _client;
+
+    [SetUp]
+    public void Setup()
+    {
+        _client = new ApiClient("https://jsonplaceholder.typicode.com");
+    }
+
+    [Test]
+    public async Task GetUsers_ShouldReturnUsersList()
+    {
+        // Act
+        var response = await _client.GetAsync("/users");
+
+        // Assert status
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var content = await response.Content.ReadAsStringAsync();
+
+        var users = JsonConvert.DeserializeObject<List<User>>(content);
+
+        users.Should().NotBeNull();
+        users.Count.Should().BeGreaterThan(0);
+    }
+}
