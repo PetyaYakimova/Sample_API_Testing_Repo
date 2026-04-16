5d75325dd55037fe52427afe2dd96de0b251268e
Petya Yakimova <petya_yakimova@abv.bg>
2026-04-16 08:16:12 +0300

    Add config helper class

---
+++ ApiTests/Helpers/ConfigHelper.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+
+public static class ConfigHelper
+{
+    public static IConfigurationRoot GetConfig()
+    {
+        return new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+    }
+}
