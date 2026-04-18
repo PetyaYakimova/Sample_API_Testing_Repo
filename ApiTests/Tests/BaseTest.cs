8e881ef6b3800d2bf21e94e52d1b1780317dcf28
Petya Yakimova <petya_yakimova@abv.bg>
2026-04-18 10:15:32 +0300

    Add base test class

---
+++ ApiTests/Tests/BaseTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+public class BaseTest
+{
+    protected ApiClient Client;
+
+    [SetUp]
+    public void BaseSetup()
+    {
+        var config = ConfigHelper.GetConfig();
+        var baseUrl = config["ApiSettings:BaseUrl"];
+
+        Client = new ApiClient(baseUrl);
+    }
+}
