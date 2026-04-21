1e6b6006b5500dee0e439e289a17775611b5cf4b
Petya Yakimova <petya_yakimova@abv.bg>
2026-04-21 08:31:43 +0300

    Add response helper

---
+++ ApiTests/Helpers/ResponseHelper.cs
@@ -0,0 +1,9 @@
+﻿using Newtonsoft.Json;
+
+public static class ResponseHelper
+{
+    public static T Deserialize<T>(string content)
+    {
+        return JsonConvert.DeserializeObject<T>(content);
+    }
+}
