c10df3520d601dc615cd0cefd1b9c8e5f0615ec7
Petya Yakimova <petya_yakimova@abv.bg>
2026-04-27 18:22:44 +0300

    Add service provider factory

---
+++ ApiTests/Helpers/ServiceProviderFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+public static class ServiceProviderFactory
+{
+    public static ServiceProvider Create()
+    {
+        var services = new ServiceCollection();
+
+        var config = ConfigHelper.GetConfig();
+        var baseUrl = config["ApiSettings:BaseUrl"];
+
+        services.AddSingleton(new ApiClient(baseUrl));
+
+        return services.BuildServiceProvider();
+    }
+}
