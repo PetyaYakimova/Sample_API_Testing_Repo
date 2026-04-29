f59409cd9078803772087b78aa0054ae120397dc
Petya Yakimova <petya_yakimova@abv.bg>
2026-04-29 08:51:08 +0300

    Add user client

---
+++ ApiTests/Clients/UserClient.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+public class UserClient
+{
+    private readonly ApiClient _apiClient;
+
+    public UserClient(ApiClient apiClient)
+    {
+        _apiClient = apiClient;
+    }
+
+    public async Task<HttpResponseMessage> GetUsers()
+    {
+        return await _apiClient.GetAsync("/users");
+    }
+
+    public async Task<HttpResponseMessage> GetUser(int id)
+    {
+        return await _apiClient.GetAsync($"/users/{id}");
+    }
+
+    public async Task<HttpResponseMessage> CreateUser(string body)
+    {
+        return await _apiClient.PostAsync("/users", body);
+    }
+}
