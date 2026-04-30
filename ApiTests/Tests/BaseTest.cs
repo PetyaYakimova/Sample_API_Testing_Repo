f004db1a1e04e49bc38ac507e7a1b4f85875411c
Petya Yakimova <petya_yakimova@abv.bg>
2026-04-30 08:44:04 +0300

    Add userclient is base test

--- ApiTests/Tests/BaseTest.cs
+++ ApiTests/Tests/BaseTest.cs
@@ -3,11 +3,14 @@
 public class BaseTest
 {
     protected ApiClient Client;
+    protected UserClient UserClient;
 
     [SetUp]
     public void Setup()
     {
         var provider = ServiceProviderFactory.Create();
+
         Client = provider.GetService<ApiClient>();
+        UserClient = provider.GetService<UserClient>();
     }
 }
