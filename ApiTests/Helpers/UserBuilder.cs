691fa36f5a2a42caa8bb37e51838428df84b713d
Petya Yakimova <petya_yakimova@abv.bg>
2026-05-01 10:28:42 +0300

    Add user builder

---
+++ ApiTests/Helpers/UserBuilder.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+
+public class UserBuilder
+{
+    private string _name = "Default Name";
+    private string _username = "defaultuser";
+    private string _email = "default@test.com";
+
+    public UserBuilder WithName(string name)
+    {
+        _name = name;
+        return this;
+    }
+
+    public UserBuilder WithEmail(string email)
+    {
+        _email = email;
+        return this;
+    }
+
+    public string Build()
+    {
+        var obj = new
+        {
+            name = _name,
+            username = _username,
+            email = _email
+        };
+
+        return JsonConvert.SerializeObject(obj);
+    }
+}
