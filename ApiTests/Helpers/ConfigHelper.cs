2deeb80aaf53b8572e966e5d8daf42574dd5901b
Petya Yakimova <petya_yakimova@abv.bg>
2026-05-03 10:56:31 +0300

    Update config helper

--- ApiTests/Helpers/ConfigHelper.cs
+++ ApiTests/Helpers/ConfigHelper.cs
@@ -4,9 +4,12 @@ public static class ConfigHelper
 {
     public static IConfigurationRoot GetConfig()
     {
+        var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
+
         return new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
             .AddJsonFile("appsettings.json")
+            .AddJsonFile($"appsettings.{environment}.json", optional: true)
             .Build();
     }
 }
