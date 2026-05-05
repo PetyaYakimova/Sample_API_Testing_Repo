8ee44be5d749f615bb6a3d5d089f2d84f20397b8
Petya Yakimova <petya_yakimova@abv.bg>
2026-05-05 08:50:13 +0300

    Add retry policies

---
+++ ApiTests/Helpers/RetryPolicies.cs
@@ -0,0 +1,22 @@
+﻿using Polly;
+using Polly.Extensions.Http;
+using System;
+using System.Net.Http;
+
+public static class RetryPolicies
+{
+    public static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+    {
+        return HttpPolicyExtensions
+            .HandleTransientHttpError() // 5xx + network errors
+            .OrResult(msg => !msg.IsSuccessStatusCode)
+            .WaitAndRetryAsync(
+                retryCount: 3,
+                sleepDurationProvider: retryAttempt =>
+                    TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                onRetry: (outcome, timespan, retryAttempt, context) =>
+                {
+                    Console.WriteLine($"Retry {retryAttempt} after {timespan.TotalSeconds}s");
+                });
+    }
+}
