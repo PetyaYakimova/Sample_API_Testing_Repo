53fb9965f7ace1f4283c2bf88ec2b2d918a585bd
Petya Yakimova <petya_yakimova@abv.bg>
2026-05-06 09:19:20 +0300

    Update service provider factory

--- ApiTests/Helpers/ServiceProviderFactory.cs
+++ ApiTests/Helpers/ServiceProviderFactory.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using Polly;
+using Polly.Extensions.Http;
 
 public static class ServiceProviderFactory
 {
@@ -9,7 +12,15 @@ public static class ServiceProviderFactory
         var config = ConfigHelper.GetConfig();
         var baseUrl = config["ApiSettings:BaseUrl"];
 
-        services.AddSingleton(new ApiClient(baseUrl));
+        // Register HttpClient with Polly
+        services.AddHttpClient<ApiClient>(client =>
+        {
+            client.BaseAddress = new Uri(baseUrl);
+            client.Timeout = TimeSpan.FromSeconds(30);
+        })
+        .AddPolicyHandler(RetryPolicies.GetRetryPolicy());
+
+        // Typed clients
         services.AddSingleton<UserClient>();
 
         return services.BuildServiceProvider();
